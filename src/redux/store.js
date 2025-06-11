e3c633e42cb6cc8de27c3a9c4e0cd364e3d30259
Volodymyr Vladykin <vova@pocketguard.com>
2025-06-11 12:26:11 +0300

    Add redux-persist usage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     contacts: contactsReducer,
     filters: filtersReducer,
-  },
+  })
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+export const persistor = persistStore(store);
